e29df49473e3bdbad47eb562d693eade020398f2
haejin <luckyhaejin1126@gmail.com>
2024-02-21 16:22:07 +0900

    설치 및 적용(router,bootstrap,axios, reactquery), Route구성, NotFound 페이지(미디어쿼리 적용)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AppLayout from './layout/AppLayout';
+import HomePage from './pages/Home/HomePage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFound/NotFoundPage';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+//홈페이지 /
+//영화 전체 보여주는 페이지(서치) /movie 검색조건이 추가되면 /movie?q=asdfadsf 이런식으로
+//영화 디테일 페이지 /movies/:id
+//추천 영화 /movies/:id/recommandation
+//리뷰 /movies/:id/reviews
 function App() {
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path="movies">
+          <Route index element={<MoviePage/>}/>
+          <Route path=":id" element={<MovieDetailPage/>}/>
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFoundPage.style.css';
+const NotFoundPage = () => {
+  return (
+    <div className='background'>
+      <div className="background-img">
+        <div className="title">404 Not Found</div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
