2a5d773d5a9e78d3e6c50c90fc613ffda3ac0916
haejin <luckyhaejin1126@gmail.com>
2024-02-22 09:34:56 +0900

    Authorization오타 수정, 배너만들기 => popular데이터 가져오기(usePopularMovies => usePopularMoviesQuery생성하여 Banner에 적용)

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
 
+/*
+1. 배너 => popular영화 들고와서 첫번째 아이템 보여주기(api호출)
+2. popular movie
+3. top rated movie
+4. upcoming movie
+*/
 const HomePage = () => {
   return (
     <div>
       HomePage
+      <Banner />
     </div>
   )
 }
